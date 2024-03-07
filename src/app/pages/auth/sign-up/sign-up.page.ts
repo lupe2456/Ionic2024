25ce0eb27112df67fb7f1b2f402d6305d7663491
lupe2456 <lupegomez2456@gmail.com>
2024-03-07 11:15:39 -0600

    first commit

---
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
+})
+export class SignUpPage implements OnInit {
+  form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
---
+++ src/app/pages/main/main.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage implements OnInit {
+  router = inject(Router);
+  currentPath: string = '';
+
+  pages = [
+          { title: 'Inicio', url: '/main/home', icon: 'home-outline' },
+          { title: 'Perfil', url: '/main/profile', icon: 'person-outline' }
+          
+          ];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: any ) =>{
+      if(event?.url) this.currentPath = event.url
+    })
+    console.log(this.pages);
+    
+  }
+}
